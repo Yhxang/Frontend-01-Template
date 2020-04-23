779debd18702f4d6005eebce0fdd2ac81e7086d1
yhxang <yhxang@163.com>
2020-04-23 11:37:57 +0800

    utf-encoding添加超出转码范围错误情况

--- week02/utf_encoding.js
+++ week02/utf_encoding.js
@@ -3,18 +3,19 @@ function UTF8_Encoding(string){
     let result = '';
     for (let i = 0; i < len; i++){
         let cp = string.codePointAt(i);
-        let strbit = cp.toString(2);
-        let strutf8, bits;
-        if( cp <= 0x7F){
-            bits = strbit;
+        let bits;
+        if(cp <= 0x7F){
+            bits = cp.toString(2);
         }else if(cp <= 0x07FF){
-            bits = strbit.padStart(11,'0').replace(/(\d{5})(\d{6})/,'110$110$2');
+            bits = cp.toString(2).padStart(11,'0').replace(/(\d{5})(\d{6})/,'110$110$2');
         }else if(cp <= 0xFFFF){
-            bits = strbit.padStart(16,'0').replace(/(\d{4})(\d{6})(\d{6})/,'1110$110$210$3');
+            bits = cp.toString(2).padStart(16,'0').replace(/(\d{4})(\d{6})(\d{6})/,'1110$110$210$3');
+        }else if(cp <= 0x10FFFF){
+            bits = cp.toString(2).padStart(21,'0').replace(/(\d{3})(\d{6})(\d{6})(\d{6})/,'11110$110$210$310$4');
         }else{
-            bits = strbit.padStart(21,'0').replace(/(\d{3})(\d{6})(\d{6})(\d{6})/,'11110$110$210$310$4');
+            throw Error('超出转码范围')
         }
-        result += parseInt(bits,2).toString(16).match(/[0-9a-fA-F]{2}/g).map(e=>'\\x' + e.toUpperCase()).join('');
+        result += parseInt(bits,2).toString(16).match(/[0-9a-fA-F]{2}/g).map(e => '\\x' + e.toUpperCase()).join('');
     } 
     return result;
 }
