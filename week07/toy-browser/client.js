33da88dc9a1c2555298d7154fccb29a00c905340
yhxang <yhxang@163.com>
2020-05-28 11:31:26 +0800

    fix: render

--- week07/toy-browser/client.js
+++ week07/toy-browser/client.js
@@ -229,8 +229,8 @@ void async function(){
 
     // 借助images库，渲染成位图
     let viewport = images(800, 600);
-    render(viewport, dom.children[0].children[3].children[1].children[3])
-    //render(viewport, dom);
+    //render(viewport, dom.children[0].children[3].children[1].children[3])
+    render(viewport, dom);
     
     viewport.save("viewport.jpg");
 }()
