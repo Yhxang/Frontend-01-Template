ed75b45756c95dff0b155b5d8a9361cd73bceddd
Yhxang <yhxang@163.com>
2020-07-16 14:48:31 +0800

    feat: carousel初步组件化

--- week14/component/main.js
+++ week14/component/main.js
@@ -1,150 +1,7 @@
-//require("./foo.js")
-//import "./foo"
+import {createElement, Text, Wrapper} from "./createElement";
 
-function createElement(Cls, attributes, ...children){
-    // console.log(arguments)
-    // debugger
-    let o
-    if(typeof Cls == "string"){
-        o = new Wrapper(Cls)
-    }else{
-        o = new Cls({
-            timer: {}
-        });
-    }
-
-    
-    for(let name in attributes){
-        //o[name] = attributes[name]; // 用property
-        o.setAttribute(name, attributes[name]); //用attribute
-    }
-    //console.log(children)
-    let visit = (children) => {
-        for(let child of children){
-            //o.children.push(child);
-            if(typeof child == "string")
-                child = new Text(child);
-
-            if(Array.isArray(child)){
-                visit(child);
-                continue;
-            }
-            o.appendChild(child);
-        }
-    }
-
-    visit(children);
-
-    return o;
-}
-
-class Wrapper{
-    constructor(type){
-        //console.log("config", config);
-        this.children = [];
-        this.root = document.createElement(type);
-    }
-
-    set class(v){ // property
-        console.log("Wrapper::class", v); //在 o[name] = attributes[name] 时触发
-    }
-
-    set id(v){
-        console.log("Wrapper::id", v);
-    }
-
-    setAttribute(name, value){ // attribute
-        //console.log("Parent::setAttribute", name, value)
-        this.root.setAttribute(name, value);
-    }
-
-    mountTo(parent){
-        parent.appendChild(this.root);
-
-        for(let child of this.children){
-            if(typeof child === "string")
-                child = new Text(child)
-            child.mountTo(this.root);
-        }
-    }
-
-    appendChild(child){ // children
-        console.log("Wrapper::appendChild", child)
-        //this.root.appendChild(child);
-        //child.mountTo(this.root);
-        this.children.push(child);
-    }
-
-    addEventListener(){
-        this.root.addEventListener(...arguments);
-    }
-}
-class Text{
-    constructor(text){
-        this.root = document.createTextNode(text);
-    }
-    mountTo(parent){
-        parent.appendChild(this.root);
-    }
-}
-
-/*
-class MyComponent{
-    constructor(config){
-        //console.log("config", config);
-        this.children = [];
-        this.attributes = new Map();
-        this.properties = new Map();
-    }
-
-    set class(v){ // property
-        console.log("Parent::class", v); //在 o[name] = attributes[name] 时触发
-    }
-
-    set id(v){
-        console.log("Parent::id", v);
-    }
-
-    setAttribute(name, value){ // attribute
-        this.attributes.set(name, value);
-    }
-
-    set title(value){
-        this.properties.set("title", value);
-    }
-
-    render(){
-        //this.slot = <div></div>
-        return <article>
-            <h1>{this.attributes.get("title")}</h1>
-            <h2>{this.properties.get("title")}</h2>
-            <header>I'm a header</header>
-            {this.slot}
-            <footer>I'm a footer</footer>
-        </article>
-    }
-
-    appendChild(child){ // children
-        console.log("Parent::appendChild", child)
-        //this.slot.appendChild(child);
-        this.children.push(child)
-    }
-    mountTo(parent){
-        this.slot = <div></div>
-        for(let child of this.children){
-            if(typeof child === "string"){
-                child = new Text(child)
-            }
-                
-            this.slot.appendChild(child)
-        }
-        this.render().mountTo(parent);
-    }
-}
-*/
 class Carousel{
     constructor(config){
-        //console.log("config", config);
         this.children = [];
         this.attributes = new Map();
         this.properties = new Map();
@@ -155,31 +12,94 @@ class Carousel{
     }
 
     render(){
-        return <div class="carousel">
-            { this.data.map(url => {
-                let element = <img src={url}/>;
-                element.addEventListener("dragstart", event => event.preventDefault());
-                return element; 
-            }) }
-        </div>
+        let children = this.data.map(url => {
+            let element = <img src={url}/>;
+            element.addEventListener("dragstart", event => event.preventDefault());
+            return element; 
+        });
+
+        let root = <div class="carousel">
+            { children }
+        </div>;
+
+        let position = 0;
+        let nextPic = () => {
+            let nextPosition = (position + 1) % this.data.length; // 取余运算
+            
+            let current = children[position];
+            let next = children[nextPosition];
+            children.forEach(ele => {
+                ele.classList.remove("current");
+                ele.classList.remove("next");
+            })
+            current.style.transition = next.style.transition = "ease 0s";
+            current.style.transform = `translateX(${- 100 * position}%)`;
+            next.style.transform = `translateX(${100 - 100 * nextPosition}%)`; 
+
+            setTimeout(function(){
+                current.style.transition = next.style.transition = "";
+
+                current.style.transform = `translateX(${- 100 - 100 * position}%)`;
+                current.classList.add('current');
+                next.style.transform = `translateX(${- 100 * nextPosition}%)`;
+                next.classList.add('next');
+
+                position = nextPosition;
+            }, 16)
+
+            setTimeout(nextPic, 3000);
+        }
+        //setTimeout(nextPic, 3000);
+
+        root.addEventListener("mousedown", event => {
+            let nextPosition = (position + 1) % this.data.length; 
+            let lastPosition = (position - 1 + this.data.length) % this.data.length;
+
+            let current = children[position];
+            let next = children[nextPosition];
+            let last = children[lastPosition];
+
+            current.style.transition =  next.style.transition = last.style.transition = "ease 0s";
+            let startX = event.clientX, startY = event.clientY;
+
+            let move = event => {
+                //console.log(event.clientX - startX, event.clientY - startY)
+                current.style.transform = `translateX(${event.clientX - startX - 500 * position}px)`
+                next.style.transform = `translateX(${event.clientX - startX + 500 - 500 * nextPosition}px)`
+                last.style.transform = `translateX(${event.clientX - startX - 500 - 500 * lastPosition}px)`
+            }
+            let up = event => {
+
+                let offset = 0;
+                if(event.clientX - startX > 250){
+                    offset = 1;
+                }else if(event.clientX - startX < -250){
+                    offset = -1;
+                }
+
+                current.style.transition =  next.style.transition = last.style.transition = "";
+
+                current.style.transform = `translateX(${offset * 500 - 500 * position}px)`
+                next.style.transform = `translateX(${offset * 500 + 500 - 500 * nextPosition}px)`
+                last.style.transform = `translateX(${offset * 500 - 500 - 500 * lastPosition}px)`
+
+                position = (position - offset + this.data.length) % this.data.length;
+
+                document.removeEventListener("mousemove", move);
+                document.removeEventListener("mouseup", up);
+            }
+            document.addEventListener("mousemove", move);
+            document.addEventListener("mouseup", up);
+        })
+        return root;
     }
 
     mountTo(parent){
         this.render().mountTo(parent);
     }
 }
-// let component = <div id="a" class="b" style="width:100px; height:100px; background-color:green;">
-//     <div></div>
-//     <p>123</p>
-//     <div></div>
-//     <div></div>
-//     </div>
 
-// let component = <div>{new Wrapper('span')}</div>
 
-// let component = <MyComponent>
-//         <div>{new Wrapper('span')}</div>
-//     </MyComponent>
 let component = <Carousel data={[
     "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
     "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
